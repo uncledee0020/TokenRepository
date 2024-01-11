c366047cfe729966592d014f9c771a56cde8b72c
Donard Ndali Musili <donardndali00@gmail.com>
2024-01-11 10:55:30 +0300

    Adding files

---
+++ src/main/java/com/ncba/tokenrepo/TokenrepoApplication.java
@@ -0,0 +1,13 @@
+package com.ncba.tokenrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ncba/tokenrepo/TokenrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncba.tokenrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
